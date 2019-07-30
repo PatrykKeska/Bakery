7bf3073ce2a44a8d77b73bfe69814663f624c457
Patryk Keska <patrykkeska4@gmail.com>
2019-07-30 13:57:18 +0200

    project uploaded on gh pages

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import MainTemplate from '../templates/MainTemplate';
 import MainPage from '../views/MainPage';
-import { BrowserRouter, Switch, Route,} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route,} from 'react-router-dom';
 import ContactPage from '../views/ContactPage';
 // import GalleryPage from '../views/GalleryPage'; 
 import LightboxPage from '../views/LightboxPage';
 function Root() {
 
   return (
-  <BrowserRouter>
+  <Router>
   <MainTemplate>
   <Switch>
-    <Route path='/' exact component={MainPage} />
+    <Route exact path='/' component={MainPage} />
     <Route path='/kontakt' component={ContactPage} />
     <Route path='/galeria' component={LightboxPage} />
   </Switch>
   </MainTemplate>
-  </BrowserRouter>
+  </Router>
 
 
   );
